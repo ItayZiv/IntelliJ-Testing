f16e6f1cc65524809cdf3f091d972dfc9510ce79
Itay Ziv <itay220204@gmail.com>
2019-12-06 03:09:18 +0200

    Test

--- src/main/java/frc/robot/commands/CloseHatch.java
+++ src/main/java/frc/robot/commands/CloseHatch.java
@@ -23,7 +23,7 @@ public class CloseHatch extends Command {
     protected boolean isFinished() {
         return false;
     }
-
+//
     // Called once after isFinished returns true
     @Override
     protected void end() {
