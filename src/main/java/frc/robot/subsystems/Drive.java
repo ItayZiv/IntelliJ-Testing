4cb93a091151e99fb6696b501eff574747bbcc99
Itay Ziv <itay220204@gmail.com>
2019-12-06 03:06:02 +0200

    Initial Commit

---
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -0,0 +1,16 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Drive extends Subsystem {
+
+
+    public Drive() {
+
+    }
+
+    @Override
+    public void initDefaultCommand() {
+
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/Hatch.java
@@ -0,0 +1,15 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Hatch extends Subsystem {
+
+    public Hatch() {
+
+    }
+
+    @Override
+    public void initDefaultCommand() {
+
+    }
+}
