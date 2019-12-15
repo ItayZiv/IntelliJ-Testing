c51f6c0184fbfcb0725a7bb389c737f318f94e6a
epicDISZ <itay220204@gmail.com>
2019-12-15 10:30:11 +0000

    Add vision subsystem

---
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -0,0 +1,14 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Vision extends Subsystem {
+
+    public Vision() {
+    }
+
+    @Override
+    protected void initDefaultCommand() {
+        
+    }
+}
